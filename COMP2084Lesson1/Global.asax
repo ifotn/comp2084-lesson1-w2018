3c7dc260ea4beb727fa2060a6952baf8c0fea6f0
ifotn <rich@infrontofthenet.com>
2019-01-09 13:50:22 -0500

    end lesson 1

---
+++ COMP2084Lesson1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2084Lesson1.MvcApplication" Language="C#" %>
---
+++ COMP2084Lesson1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2084Lesson1.Startup))]
+namespace COMP2084Lesson1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
